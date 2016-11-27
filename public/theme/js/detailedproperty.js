85047ed75c9229856a5f097673fd4552d64002b4
Shubham yeole <shubham20.yeole@gmail.com>
2016-11-27 12:42:24 -0500

    Update sign in functionality

--- public/theme/js/detailedproperty.js
+++ public/theme/js/detailedproperty.js
@@ -1,8 +1,16 @@
 $( document ).ready(function() {
+	$("#contactdiv").hide();
     var features = $("#features").val();
 	var x = features.split(','); // have to quote regular expressions with /
 	var xL = x.length;
 	for(i=0; i<xL; i++){
 		$('#append').append('<li>'+x[i]+'</li>'); // do what
 	}
+	$("#ctnprpbtn").click(function() {
+
+		$("#contactdiv").before("<br><br><br>");
+		$("#contactdiv").show().addClass("animated swing");
+
+		$("#clicktoscroll").click();
+	});
 });
